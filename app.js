8e6f4798423bc9b6a8f6af3308701ae984d17677
Rodrigo Gomes <94005241+ro03@users.noreply.github.com>
2025-07-26 19:28:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const { App } = require('@slack/bolt');
 const express = require('express');
 const dotenv = require('dotenv');
