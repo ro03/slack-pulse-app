cecc5921ad8e9c3f1c3974c721224f323eb386bd
Rodrigo Gomes <94005241+ro03@users.noreply.github.com>
2025-07-27 18:54:02 +0200

    Update app.js (without socket)

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const { saveResponseToSheet } = require('./sheets');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+  //socketMode: true,
+  //appToken: process.env.SLACK_APP_TOKEN
 });
 
 // This command opens the poll creation form (modal)
